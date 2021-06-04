1a3bfbcf4b5e9d29416610871f541e4776c55c7e
zafercobannn <zafercoban6699@gmail.com>
2021-06-04 13:05:21 +0300

    9.Gün Ödevi

--- src/main/java/com/example/hrmsproject/Api/Conrollers/ForeignLanguagesService.java
+++ src/main/java/com/example/hrmsproject/Api/Conrollers/ForeignLanguagesService.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/foreignlanguage")
+@RequestMapping("/api/foreignlanguages")
 public class ForeignLanguagesService {
     private ForeignLanguageForCVService foreignLanguageForCVService;
 
